6b2576b4a99e10bc7a3fe0138037ef7f1535ad63
hyf <hhhouyifu@gmail.com>
2025-03-26 17:26:42 +0800

    Initial commit

---
+++ pj1/build/CMakeFiles/a1.dir/DependInfo.cmake
@@ -0,0 +1,53 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/hyf/graphics/pj1/glew/src/glew.c" "/home/hyf/graphics/pj1/build/CMakeFiles/a1.dir/glew/src/glew.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "GLEW_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../vecmath/include"
+  "../glfw/include"
+  "../glew/include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hyf/graphics/pj1/src/camera.cpp" "/home/hyf/graphics/pj1/build/CMakeFiles/a1.dir/src/camera.cpp.o"
+  "/home/hyf/graphics/pj1/src/curve.cpp" "/home/hyf/graphics/pj1/build/CMakeFiles/a1.dir/src/curve.cpp.o"
+  "/home/hyf/graphics/pj1/src/main.cpp" "/home/hyf/graphics/pj1/build/CMakeFiles/a1.dir/src/main.cpp.o"
+  "/home/hyf/graphics/pj1/src/parse.cpp" "/home/hyf/graphics/pj1/build/CMakeFiles/a1.dir/src/parse.cpp.o"
+  "/home/hyf/graphics/pj1/src/starter1_util.cpp" "/home/hyf/graphics/pj1/build/CMakeFiles/a1.dir/src/starter1_util.cpp.o"
+  "/home/hyf/graphics/pj1/src/surf.cpp" "/home/hyf/graphics/pj1/build/CMakeFiles/a1.dir/src/surf.cpp.o"
+  "/home/hyf/graphics/pj1/src/vertexrecorder.cpp" "/home/hyf/graphics/pj1/build/CMakeFiles/a1.dir/src/vertexrecorder.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLEW_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../vecmath/include"
+  "../glfw/include"
+  "../glew/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/hyf/graphics/pj1/build/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/home/hyf/graphics/pj1/build/vecmath/CMakeFiles/vecmath.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
